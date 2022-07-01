abd9a091942131fd05aeb6a426a1eeb862687e61
uchabokeria <ucha1bokeria@gmail.com>
2022-07-01 14:48:41 +0400

    fx

--- Modules/Learner/Learner.Configs.php
+++ Modules/Learner/Learner.Configs.php
@@ -4,7 +4,7 @@
 Learn('Configs', "json", function($dir,$filename) {
     $parameters = json_decode(file_get_contents("./$dir/$filename"),true);
     foreach ($parameters as $key => $value) define(strtoupper($key), $value);
-    define('payment_required', ['1'=>2]);
+    define('payment_required', 1);
     
     var_dump(payment_required); 
 });
