05593afe336510c6d8c5f99b9a7c6f9500d9b430
uchabokeria <ucha1bokeria@gmail.com>
2022-07-01 15:53:19 +0400

    fx

--- Modules/Learner/Learner.Configs.php
+++ Modules/Learner/Learner.Configs.php
@@ -3,5 +3,5 @@
 /* Learn Configs */
 Learn('Configs', "json", function($dir,$filename) {
     $parameters = json_decode(file_get_contents("./$dir/$filename"),true);
-    foreach ($parameters as $key => $value) define(strtoupper($key), json_encode($value));
+    foreach ($parameters as $key => $value) define(strtoupper($key), ($value));
 });
