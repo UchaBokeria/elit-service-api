7c266977e1321f95138fd7f9b62ad7299ecf0add
uchabokeria <ucha1bokeria@gmail.com>
2022-07-01 14:30:48 +0400

    fx

--- Modules/Learner/Learner.JsonResults.php
+++ Modules/Learner/Learner.JsonResults.php
@@ -35,6 +35,4 @@ if(!method_exists($Router, $Request))
 
 }
 
-define( 'API_RESULT_JSON' , 
-    array_merge(
-        array( "result" => $Router->$Request() ), SUCCESS_STATUS) );
+define( 'API_RESULT_JSON' , array_merge( array( "result" => $Router->$Request() ), SUCCESS_STATUS) );
