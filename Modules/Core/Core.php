83eae450b39aa77e1cf27db8616ac24281ebbed5
uchabokeria <ucha1bokeria@gmail.com>
2022-07-01 14:46:27 +0400

    fx

--- Modules/Core/Core.php
+++ Modules/Core/Core.php
@@ -1,8 +1,6 @@
 <?php
 
 /* LEARNERS */
-require './Modules/Logger/Logger.php';
-require './Modules/Database/Database.php';
 require './Modules/Learner/Learner.IPaddresses.php';
 require './Modules/Learner/Learner.RequestHeader.php';
 require './Modules/Learner/Learner.RequestParmas.php';
@@ -10,6 +8,8 @@ require './Modules/Learner/Learner.Auto.php';
 require './Modules/Learner/Learner.Configs.php';
 require './Modules/Learner/Learner.ErrorReport.php';
 require './Modules/Learner/Learner.LibrariesFuncs.php';
+require './Modules/Logger/Logger.php';
+require './Modules/Database/Database.php';
 require './Modules/Mailer/SmtpMailer.php';
 require './Modules/Guardian/Guardian.php';
 require './Modules/Decorator/Decorator.php';
--- Modules/Learner/Learner.JsonResults.php
+++ Modules/Learner/Learner.JsonResults.php
@@ -1,14 +1,11 @@
 <?php
 
 $URI = parse_url(explode('?',$_SERVER['REQUEST_URI'])[0], PHP_URL_PATH);
-
 $CALL = explode("/", $URI);
 $EUPES = array_count_values($CALL)[""];
-
 for ($i=0; $i < $EUPES; $i++)  unset($CALL[array_search("",$CALL)]);
 
 $callName = str_replace('-','',$CALL[COUNT($CALL)-1]);
-
 $Request = str_replace('-','',$CALL[COUNT($CALL)]);
 
 if (!class_exists($callName)) 
@@ -18,19 +15,11 @@ if (!class_exists($callName))
     {
         
         header('http/1.1 404 Not Found');
-
-        die(
-            json_encode([
-                "code" => "404",
-                "success" => false,
-                "msg" => "You are not trying to access invalid route"
-            ]
-        ));
+        die(json_encode(NOT_FOUND));
         
     } 
    
     $callName = str_replace('-','',$CALL[COUNT($CALL)-2]);
-
     $Request = str_replace('-','',$CALL[COUNT($CALL)-1]);
 
     define('FINDBYURLPARAMETER' ,$CALL[COUNT($CALL)]);
@@ -38,23 +27,14 @@ if (!class_exists($callName))
 }
 
 $Router = new $callName();
-
 if(!method_exists($Router, $Request)) 
 {
 
     header('http/1.1 404 Not Found');
-
-    die(
-        json_encode([
-            "code" => "404",
-            "success" => false,
-            "msg" => "You are not trying to access invalid route"
-        ]
-    ));
+    die(json_encode(NOT_FOUND));
 
 }
 
-$API_RESULT_JSON = array_merge( 
-    [ "result" => $Router->$Request() ], 
-    [ "code" => "200", "success" => true, "msg" => "Success" ]
-);
+define( 'API_RESULT_JSON' , 
+    array_merge(
+        [ "result" => $Router->$Request() ], SUCCESS_STATUS) );
