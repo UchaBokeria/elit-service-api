66c33812187d5468f0b3b0a8108bf6e836a29c12
uchabokeria <ucha1bokeria@gmail.com>
2022-07-08 13:05:07 +0400

    fx

--- API/Server/Calls/Calls.Controller.php
+++ API/Server/Calls/Calls.Controller.php
@@ -24,13 +24,13 @@ class Calls extends Database
                                             FROM_UNIXTIME(call_datetime) AS datetime,
                                             IF(call_type_id = 1, source, destination) AS phone,   
                                             CONCAT(
-                                                'http://192.168.4.250:8000/', DATE(FROM_UNIXTIME(asterisk_call_log..call_datetime)), '/', asterisk_call_record.name,'.', asterisk_call_record.format
+                                                'http://192.168.4.250:8000/', DATE(FROM_UNIXTIME(asterisk_call_log.call_datetime)), '/', asterisk_call_record.name,'.', asterisk_call_record.format
                                             ) AS record
                                             
                                 FROM        asterisk_call_log
                                 LEFT JOIN   asterisk_call_record ON asterisk_call_log_id = asterisk_call_log.id
                                 WHERE       IF(call_type_id = 1, source like :source, destination like :destination); ",
-                                [ 'source' => "%$_POST[phone]%", 'destination' => "%$_POST[phone]%" ],true );
+                                [ 'source' => "%$_POST[phone]%", 'destination' => "%$_POST[phone]%" ] );
 
     }
 
